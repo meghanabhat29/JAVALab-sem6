e6fda07c80541e1ebae7ee91a6168e376647f6b9
Meghana Bhat <42279550+meghanabhat29@users.noreply.github.com>
2020-03-11 23:20:47 +0530

    Create MainClass.java

---
+++ Telephone/MainClass.java
@@ -0,0 +1,118 @@
+package telephone;
+
+import java.io.*;
+import java.util.*;
+
+public class MainClass
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in)
+				;
+		HashMap<String,String> calls=new HashMap<String,String>();
+		calls.put("9900565593", "Pappa");
+		calls.put("9741366563","Mom");
+		calls.put("7795101595","Me");
+		calls.put("8197652968","Yashu");
+		
+		ArrayList<MissedCall> al=new ArrayList<MissedCall>();
+		int i=0;
+		while(true)
+		{
+			System.out.println("Enter\n1:Call\n2:Display\n3:Delete based on request\n4:Display all details\n5:Exit\nEnter choice : ");
+			int ch;
+			ch=sc.nextInt();
+			
+			switch(ch)
+			{
+			case 1:
+				System.out.println("Enter the time of call : ");
+				String time=sc.nextLine();
+				System.out.println("Enter the number : ");
+				String number=sc.nextLine();
+				String name;
+				if(calls.containsKey(number))
+				{
+					name=calls.get(number);
+				}
+				else
+				{
+					name="Private caller";
+				}
+				System.out.println("Incoming call : "+name);
+				MissedCall mc=new MissedCall(name,time,number);
+				
+				if(al.size()==10)
+				{
+					al.remove(0);
+					al.add(mc);
+				}
+				else
+				{
+					al.add(mc);
+				}
+				break;
+			case 2:
+				int index=0;
+				for(i=al.size()-1;i>=0;i--)
+				{
+					System.out.println(al.get(i).number);
+					System.out.println("\nEnter\n1:Delete\n2:Next all\n3:Display details\n");
+					int c=sc.nextInt();
+					switch(c)
+					{
+					case 1:
+						al.remove(i);
+						break;
+					case 2:
+						continue;
+					case 3:
+						System.out.println(al.get(i));
+						break;
+					}
+					
+				}
+				break;
+			case 3:
+				System.out.println("Enter the number to be deleed from the list : ");
+				String num=sc.nextLine();
+				MissedCall rem=null;
+				for(MissedCall m:al)
+				{
+					if(m.number.equals(num))
+					{
+						rem=m;
+					}
+				}
+				al.remove(rem);
+				break;
+			case 4:
+				for(MissedCall m:al)
+				{
+					System.out.println(m);
+				}
+				break;
+				
+			case 5:
+				System.exit(0);
+				
+			}
+		}
+	}
+}
+
+class MissedCall
+{
+	String name,number,time;
+	MissedCall(String name,String number,String time)
+	{
+		this.name=name;
+		this.number=number;
+		this.time=time;
+	}
+	public String toString()
+	{
+		return name+"\t"+number+"\t"+time;
+	}
+
+}
